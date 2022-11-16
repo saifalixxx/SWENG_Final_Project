631798fbfe1a0a8c6a0ad6686c54501462cff6c0
loconno5 <loconno5@tcd.ie>
2022-11-16 18:29:19 +0000

    adding graph classes

---
+++ graphClasses/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import ApexChart from './ApexChart';
+import ApexBar from './ApexBar';
+
+
+
+function App() {
+  return (
+    <div>
+    <ApexChart/>,
+    <ApexBar/>
+    </div>
+  );
+}
+
+export default App;
