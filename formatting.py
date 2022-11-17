229ce0ed4bce5217a44e349801ed4cdf5fe389be
Wlin29 <wlin@tcd.ie>
2022-11-17 00:27:40 +0000

    added feature to get the number of commits per person

--- formatting.py
+++ formatting.py
@@ -5,13 +5,30 @@ import json
 # Get the number of commits per person from a given file
 def get_commit_info(filename):
     # Create a dictionary with commiter name and number of commits as key value pairs
-    dict = {}
-    with open(filename,"r") as commits_data:
-        data = json.loads(commits_data) # Possibly json.load, no idea what I am doing
-        dict = data["name"]
-        return dict
+    commits_per_author = {}
+    
+    # Read in JSON file
+    with open(filename,'r') as file:
+
+    # Return JSON object as a dictionary
+        data = json.load(file)
+
+    # Narrow down the json file until it is just a name representing a commit
+        commit_list = [x['commit'] for x in data]
+        author_list = [x['author'] for x in commit_list]
+        name_list = [x['name'] for x in author_list]
+
+        # Loop through the list of names and count how many instances there are of each name
+        for name in name_list:
+            if name in commits_per_author:
+                commits_per_author[name] += 1
+            else:
+                commits_per_author.update({name: 1})
+
+    return commits_per_author  
 
 
 if __name__=='__main__':
     filename = "commits.json"
-    print(get_commit_info(filename))
+    dict = get_commit_info(filename)
+    print(dict)
