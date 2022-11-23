4e8d75b4fef4c5fcf4d2f589a6e4cf840a3fed81
ryanido <ryanidowu@gmail.com>
2022-11-23 17:52:22 +0000

    added json files for testing

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import SideBar from './Components/Sidebar';
 import Graph from './Components/Graph';
-
 import './App.css';
+import commits from './test_files/formatted_commits'
+import commitsPerPerson from './test_files/other_one.json'
+import churns from './test_files/code_churn.json'
+import languages from './test_files/languages.json'
 
 export default function App() {
-  const dummy = [44, 55, 13, 43, 22]
-  const dummyLabels = ['Java', 'Python', 'C', 'Haskell', 'JavaScript']
-  const barDummy = [34,55,76]
-  const barDummyLabels = ['New Work', 'Refactor', 'Rework'] 
-  const lineDummy = [1,2,3,3,4,5,6]
-  const lineDummyLabels = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
-  const barVDummy = [3,14,1,1,4,4,1,1,1]
-  const barVDummyLabels = ["Andrew Chow","MacroFake","Ryan Ofsky","Hennadii Stepanov","Sebastian Falbesoner","fanquake","James O'Beirne","Martin Zumsande","Sjors Provoost"]
   return (
     <div id="App">
       <SideBar />
@@ -20,10 +15,10 @@ export default function App() {
         <h1>SWENG Dashboard</h1>
       </div>
       <div>
-        <Graph type='pie' data={dummy} labels = {dummyLabels} />
-        <Graph type= 'hbar' data={barDummy} labels= {barDummyLabels} />
-        <Graph type= 'line' data={lineDummy} labels= {lineDummyLabels} />
-        <Graph type= 'vbar' data={barVDummy} labels= {barVDummyLabels} />
+        <Graph type='pie' data={languages.data} labels = {languages.labels} />
+        <Graph type= 'hbar' data={churns.data} labels= {churns.labels} />
+        <Graph type= 'line' data={commitsPerPerson.data} labels= {commitsPerPerson.labels} />
+        <Graph type= 'vbar' data={commits.data} labels= {commits.labels} />
       
       </div>
     </div>
