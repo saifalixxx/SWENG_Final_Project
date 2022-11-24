506a9732beb2764619fdd6e55719d402cf8d27a5
loconno5 <loconno5@tcd.ie>
2022-11-24 02:16:35 +0000

    Changes to graph design

--- dashboard/src/Components/ApexChart.js
+++ dashboard/src/Components/ApexChart.js
@@ -4,20 +4,33 @@ import ReactApexChart from 'react-apexcharts';
 const ApexChart = ({ data, dLabels }) => {
 
   const options = {
+
     chart: {
+      title: "Commits per Day",
       width: 380,
       type: 'pie',
+      background: '#262626',
+      foreColor: 'white',
+    },
+    colors: ['#e600e6', '#33ccff', '#e6f5ff', '#ffccff', '#61DBC3'],
+    fill: {
+      type: 'colors',
+
     },
     labels: dLabels,
     responsive: [{
       breakpoint: 480,
       options: {
         chart: {
-          width: 200
+          width: 250,
+          background: '#262626',
         },
+
         legend: {
           position: 'top'
-        }
+        },
+
+
       }
     }]
   }
@@ -25,7 +38,7 @@ const ApexChart = ({ data, dLabels }) => {
 
   return (
     <div id="chart">
-      <ReactApexChart options={options} series={data} type="pie" width={380} />
+      <ReactApexChart options={options} series={data} type="pie" width={500} />
     </div>
   )
 }
