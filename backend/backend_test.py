6ed8fe6f664324cd7fe4f64b9f15d2965cde1bab
Wlin29 <wlin@tcd.ie>
2022-12-01 22:01:58 +0000

    combined test files

---
+++ backend/backend_test.py
@@ -0,0 +1,13 @@
+from API_tests import *
+from formatting_test import *
+
+# API calls tests
+test_languages()
+test_code_frequency()
+test_commit_activity()
+test_commits()
+
+# formatting tests
+test_get_commit_info()
+test_get_language_info()
+test_get_code_frequency_info()
