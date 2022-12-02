7e93d11f6ba71af19b39ee32d7d4c2059016d847
loconno5 <loconno5@tcd.ie>
2022-12-02 11:46:54 +0000

    moving to data folder

--- dashboard/src/Pages/Home.jsx
+++ dashboard/src/Pages/Home.jsx
@@ -6,19 +6,17 @@ import ApexChart from "../Components/graphs/ApexChart";
 import ApexPlot from "../Components/graphs/ApexPlot";
 import VertBar from "../Components/graphs/VertBar";
 import ApexBar from '../Components/graphs/ApexBar';
-import languages from '../languages.json'
-import percentage from '../languages.json'
-import total from '../commit_activity.json'
-import freq from '../code_frequency.json'
-import days from './active_days.json'
+import languages from './Data/languages.json'
+import percentage from './Data/languages.json'
+import total from './Data/commit_activity.json'
+import freq from './Data/code_frequency.json'
+import days from './Data/active_days.json'
 
 const Home = () => {
     const totalLinesAdded = freq["lines added"].reduce((sum, a) => sum + a, 0);
     const totalLinesDeleted = freq["lines deleted"].reduce((sum, a) => sum - a, 0);
     const totalLines = totalLinesAdded + totalLinesDeleted
     const freqData = [(totalLinesAdded / totalLines).toFixed(1) * 100, (totalLinesDeleted / totalLines).toFixed(1) * 100]
-    const barVDummy = [3, 14, 1, 1, 4, 4, 1, 1, 1]
-    const barVDummyLabels = ["Andrew Chow", "MacroFake", "Ryan Ofsky", "Hennadii Stepanov", "Sebastian Falbesoner", "fanquake", "James O'Beirne", "Martin Zumsande", "Sjors Provoost"]
     const weekLabels = ["W1", "W5", "W10", "W15", "W20", "W25", "W30", "W35", "W40", "W45", "W50"]
     const weekNos = [total[0].total, total[4].total, total[9].total, total[14].total, total[19].total, total[24].total, total[29].total, total[34].total, total[39].total, total[44].total, total[49].total]
     const activeDays = [days[0], days[4], days[9], days[14], days[19], days[24], days[29], days[34], days[39], days[44], days[49]]
