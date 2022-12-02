549c5b3fad007de10ed4a833dcc34a8a60b9b881
ryanido <ryanidowu@gmail.com>
2022-12-02 10:28:33 +0000

    some UI Changes + some individual functionality

--- dashboard/src/Pages/Home.jsx
+++ dashboard/src/Pages/Home.jsx
@@ -16,9 +16,8 @@ import freq from '../code_frequency.json'
 const Home = () => {
     const totalLinesAdded = freq["lines added"].reduce((sum, a) => sum + a, 0);
     const totalLinesDeleted = freq["lines deleted"].reduce((sum, a) => sum - a, 0);
-    const freqData = [totalLinesAdded, totalLinesDeleted]
-    const barDummy = [34, 55, 76]
-    const barDummyLabels = ['New Work', 'Refactor', 'Rework']
+    const totalLines = totalLinesAdded + totalLinesDeleted
+    const freqData = [(totalLinesAdded / totalLines).toFixed(1) * 100, (totalLinesDeleted / totalLines).toFixed(1) * 100]
     const barVDummy = [3, 14, 1, 1, 4, 4, 1, 1, 1]
     const barVDummyLabels = ["Andrew Chow", "MacroFake", "Ryan Ofsky", "Hennadii Stepanov", "Sebastian Falbesoner", "fanquake", "James O'Beirne", "Martin Zumsande", "Sjors Provoost"]
     const weekLabels = ["W1", "W5", "W10", "W15", "W20", "W25", "W30", "W35", "W40", "W45", "W50"]
@@ -28,28 +27,16 @@ const Home = () => {
         <div className="home">
             <Sidebar />
             <div className="homeContainer">
-                <Titlebar name = {"Github Metrics Dashboard"}/>
+                <Titlebar name={"Github Metrics Dashboard"} />
                 {/* Change colors pls  */}
                 <div className="stats">
-                    <div className='boxStyle' style={{background: 'linear-gradient(45deg, #0b2f4d, #94eef8)'}}>
-                        Commits Today:   15
-                    </div>
-                    <div className='boxStyle' style={{background: 'linear-gradient(45deg, #f112d7, #f4e7f6)'}}>
-                       Risk Alerts: 2
-                    </div>
-                    <div className='boxStyle' style={{background: 'linear-gradient(45deg, #0b2f4d, #94eef8)'}}>
-                        Lines Added per Commit: 20
-                    </div>
-                    <div className='boxStyle' style={{background: 'linear-gradient(45deg, #f112d7, #f4e7f6)'}}>
-                        Lines Deleted per Commit: 15 
-                    </div>
-                    <div className='boxStyle' style={{background: 'linear-gradient(45deg, #0b2f4d, #94eef8)'}}>
-                        Top Commmitter: Ryanido 
-                    </div>
+                    <a className='boxStyle' href={`https://github.com/`} style={{ background: 'linear-gradient(45deg, #0b2f4d, #94eef8)' }}>
+                            Github
+                    </a>
                 </div>
                 <div className="row1">
                     <ApexChart data={percentage.percentage} dLabels={languages.languages} />
-                    <ApexBar data={freqData} dLabels={['Lines added', 'Lines deleted']} />
+                    <ApexBar data={freqData} dLabels={['Lines added(%)', 'Lines deleted(%)']} />
                 </div>
                 <div className="row2">
                     <ApexPlot data={weekNos} dLabels={weekLabels} />
